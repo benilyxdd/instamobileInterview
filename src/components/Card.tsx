4e0d66f17e028bdbcd91e92416fb90ef392961ea
benily <leekwokhinben@gmail.com>
2021-12-18 16:08:45 +0800

    add card props interface and tests

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Card = () => {
+export interface Props {
+	imageUrl: string;
+	title: string;
+	category: string;
+}
+
+const Card: React.FunctionComponent<Props> = ({
+	imageUrl,
+	title,
+	category,
+}) => {
 	return (
 		<View>
 			<Text>Card</Text>
--- src/components/__tests__/Card.tsx
+++ src/components/__tests__/Card.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 
-import Card from '../Card';
+import Card, { Props } from '../Card';
 
-const card = create(<Card />);
+const testData: Props = {
+	imageUrl:
+		'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
+	title: 'Oatmeal Cookies',
+	category: 'Cookies',
+};
 
-it('match snapshot', () => {
-	expect(card).toMatchSnapshot();
+const card = create(<Card {...testData} />);
+
+describe('Card', () => {
+	it('matches snapshot', () => {
+		expect(card).toMatchSnapshot();
+	});
+
+	it('renders image', () => {
+		const image = card.root.findByProps({ testID: 'image' }).props;
+	});
+
+	it('renders title and category', () => {
+		const title = card.root.findByProps({ testID: 'title' }).props;
+		expect(title).toEqual('Oatmeal Cookies');
+
+		const category = card.root.findByProps({ testID: 'category' }).props;
+		expect(category).toEqual('Cookies');
+	});
 });
