86046391bc49c22330b4b2528216364de2adafc8
benily <leekwokhinben@gmail.com>
2021-12-19 00:30:21 +0800

    finish card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 
+type sizeType = 'sm' | 'md' | 'lg'; // small, medium, large -> string
 export interface Props {
 	imageUrl: string;
 	title: string;
 	category: string;
+	size: sizeType;
 }
+
+const screen = Dimensions.get('window');
 
 const Card: React.FunctionComponent<Props> = ({
 	imageUrl,
 	title,
 	category,
+	size,
 }) => {
 	return (
-		<View>
-			<Image testID="image" source={{ uri: imageUrl }} />
-			<Text testID="title">{title}</Text>
-			<Text testID="category">{category}</Text>
+		<View
+			testID="card"
+			style={{
+				...style.cardContainer,
+				...style[size],
+			}}>
+			<View style={style.imageContainer}>
+				<Image
+					testID="image"
+					style={style.image}
+					source={{ uri: imageUrl }}
+				/>
+			</View>
+			<Text testID="title" style={style.titleText}>
+				{title}
+			</Text>
+			<Text testID="category" style={style.categoryText}>
+				{category}
+			</Text>
 		</View>
 	);
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+	sm: {
+		width: screen.width * 0.4,
+		height: screen.height * 0.25,
+	},
+	md: {
+		width: screen.width * 0.9,
+		height: screen.height * 0.33,
+	},
+	lg: {
+		width: screen.width * 0.9,
+		height: screen.height * 0.5,
+	},
+
+	cardContainer: {
+		borderColor: 'gray',
+		borderWidth: 1,
+		borderRadius: 20,
+		overflow: 'hidden',
+	},
+	categoryText: {
+		textAlign: 'center',
+	},
+	image: {
+		height: '100%',
+		width: '100%',
+	},
+	imageContainer: {
+		borderRadius: 20,
+		flex: 1,
+		height: '100%',
+		width: '100%',
+		overflow: 'hidden',
+	},
+	titleText: {
+		fontWeight: 'bold',
+		fontSize: 15,
+		textAlign: 'center',
+		paddingTop: '1%',
+		paddingBottom: '2.5%',
+	},
+
+	test: {
+		backgroundColor: 'red',
+	},
+});
 
 export default Card;
--- src/components/__tests__/Card.tsx
+++ src/components/__tests__/Card.tsx
@@ -3,29 +3,56 @@ import { create } from 'react-test-renderer';
 
 import Card, { Props } from '../Card';
 
-const testData: Props = {
+const testData: Omit<Props, 'size'> = {
 	imageUrl:
 		'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
 	title: 'Oatmeal Cookies',
 	category: 'Cookies',
 };
 
-const card = create(<Card {...testData} />);
+const card = create(<Card size="md" {...testData} />);
 
 describe('Card', () => {
-	it('matches snapshot', () => {
-		expect(card).toMatchSnapshot();
+	describe('components', () => {
+		it('snapshot', () => {
+			expect(card).toMatchSnapshot();
+		});
+
+		it('renders image', () => {
+			const image = card.root.findByProps({ testID: 'image' }).props;
+		});
+
+		it('renders title and category', () => {
+			const title = card.root.findByProps({ testID: 'title' }).props;
+			expect(title.children).toEqual('Oatmeal Cookies');
+
+			const category = card.root.findByProps({
+				testID: 'category',
+			}).props;
+			expect(category.children).toEqual('Cookies');
+		});
 	});
 
-	it('renders image', () => {
-		const image = card.root.findByProps({ testID: 'image' }).props;
-	});
+	describe('style', () => {
+		it('title and category is centered', () => {
+			const title = card.root.findByProps({ testID: 'title' }).props;
+			expect(title.style).toHaveProperty('textAlign', 'center');
 
-	it('renders title and category', () => {
-		const title = card.root.findByProps({ testID: 'title' }).props;
-		expect(title.children).toEqual('Oatmeal Cookies');
+			const category = card.root.findByProps({
+				testID: 'category',
+			}).props;
+			expect(category.style).toHaveProperty('textAlign', 'center');
+		});
 
-		const category = card.root.findByProps({ testID: 'category' }).props;
-		expect(category.children).toEqual('Cookies');
+		it('has border', () => {
+			const view = card.root.findByProps({ testID: 'card' }).props;
+			expect(view.style).toHaveProperty('borderWidth');
+			expect(view.style).toHaveProperty('borderColor');
+		});
+
+		it('has border radius of 20px', () => {
+			const view = card.root.findByProps({ testID: 'card' }).props;
+			expect(view.style).toHaveProperty('borderRadius', 20);
+		});
 	});
 });
