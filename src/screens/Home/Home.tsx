5e4ca966feba1ac93df572713045a5adae985d4e
benily <leekwokhinben@gmail.com>
2021-12-19 00:39:09 +0800

    Home skeleton

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+
+import Card from '../../components/Card';
+import { recipes, categories } from '../../../constants/mockData';
+
+export interface Props {
+	recipes: typeof recipes;
+	categories: typeof categories;
+}
+
+const Home: React.FunctionComponent<Props> = ({ recipes, categories }) => {
+	return (
+		<View>
+			<FlatList
+				testID="flatlist"
+				data={recipes}
+				keyExtractor={item => item.recipeId.toString()}
+				renderItem={({ item }) => {
+					const category = categories.find(
+						category => category.id === item.categoryId,
+					)?.name;
+					if (category === undefined) {
+						throw new Error(
+							'A categoryId should be matched within the categories list',
+						);
+					}
+					return (
+						<Card
+							size="sm"
+							title={item.title}
+							imageUrl={item.photo_url}
+							category={category}
+						/>
+					);
+				}}
+			/>
+		</View>
+	);
+};
+
+export default Home;
---
+++ src/screens/Home/__tests__/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import Home from '../Home';
+import { recipes, categories } from '../../../../constants/mockData';
+
+const screen = create(<Home recipes={recipes} categories={categories} />);
+
+describe('Home Screen', () => {
+	describe('components', () => {
+		it('renders a flatlist', () => {
+			const flatlist = screen.root.findByProps({ testID: 'flatlist' });
+		});
+
+		it('throws error when categoryId is not found in the database', () => {
+			try {
+				const screenWithoutCategoriesList = create(
+					<Home recipes={recipes} categories={[]} />,
+				);
+				expect(true).toBeFalsy();
+			} catch (error: any) {
+				expect(error.message).toBe(
+					'A categoryId should be matched within the categories list',
+				);
+			}
+		});
+	});
+});
